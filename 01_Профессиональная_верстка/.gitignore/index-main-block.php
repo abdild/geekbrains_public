1d2737c3c32ffd07c25ab7f53efc04378e090fa2
aii <abdullintat@yandex.ru>
2021-03-11 13:10:34 +0300

    add contact.php

---
+++ 01_Профессиональная_верстка/.gitignore/index-main-block.php
@@ -0,0 +1,7 @@
+<div class="main centered">
+    <img class="main__img" src="../img/men.png" alt="men">
+    <div class="main__text">
+        <h1>THE BRaND</h1>
+        <h2>OF LUXErIOUS <span class="accent">FASHION</span></h2>
+    </div>
+</div>
---
+++ 01_Профессиональная_верстка/.gitignore/promo.php
@@ -0,0 +1,26 @@
+<div class="promo centered">
+    <div class="promo__top">
+        <div class="promo__top-item promo__top-left">
+            <div class="promo__text">30% oFF</div>
+            <div class="promo__theme">FOR WOMeN</div>
+        </div>
+        <div class="promo__top-item promo__top-center">
+            <div class="promo__top-item promo__top-center">
+                <div class="promo__text">HOt DEAL</div>
+                <div class="promo__theme">FOR mEN</div>
+            </div>
+        </div>
+        <div class="promo__top-item promo__top-right">
+            <div class="promo__top-item promo__top-right">
+                <div class="promo__text">NEw ARRIVALS</div>
+                <div class="promo__theme">FOR KIdS</div>
+            </div>
+        </div>
+    </div>
+    <div class="promo__bottom">
+        <div class="">
+            <div class="promo__text">LUXIROuS & TRENDY</div>
+            <div class="promo__theme">ACCESORiES</div>
+        </div>
+    </div>
+</div>
