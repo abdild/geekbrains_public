f663b60447d64b324d7bdd0523b50f249173668e
unknown <abdullin@yandex.ru>
2021-03-16 22:35:28 +0300

    q

--- 01_Профессиональная_верстка/product.php
+++ 01_Профессиональная_верстка/product.php
@@ -4,7 +4,7 @@
     <header class="centered">
         <div class="header-title">NEW ARRivALS</div>
         <div class="header-breadcrumbs">
-            HOME / MEN / <span class="accent" style=": bold">NEW ARRIVALS</span>
+            HOME / MEN / <span class="accent" style="font-weight: bold">NEW ARRIVALS</span>
         </div>
     </header>
 
