b3e5207b0b2032a92f02cb126bf37bf5f51a4723
unknown <abdullin@yandex.ru>
2021-03-24 23:00:00 +0300

    lesson-6

--- 01/subscriber.php
+++ 01/subscriber.php
@@ -1,17 +1,18 @@
-<div class="subscribe-block centered">
-    <div class="subscribe-block__item">
-        <img src="img/face.png" alt="face">
-        <div class="subscribe-block__citate">
-            “Vestibulum quis porttitor dui! Quisque viverra nunc mi, <i>a pulvinar purus condimentum</i>“
+<div class="subscriber-bg">
+    <div class="subscribe-block centered">
+        <div class="subscribe-block__item">
+            <img src="img/face.png" alt="face">
+            <div class="subscribe-block__citate">
+                “Vestibulum quis porttitor dui! Quisque viverra nunc mi, <i>a pulvinar purus condimentum</i>“
+            </div>
         </div>
-    </div>
-    <div class="subscribe-block__item">
-        <div class="subscribe-block__title-first">SUBScRIBE</div>
-        <div class="subscribe-block__title-second">FOR OUR NEWLETteR AND PROMOTION</div>
-        <div class="subscribe-block__input">
-            <input type="email" placeholder="Enter Your Email">
-            <a class="subscribe-block__button" href="#">Subscribe</a>
+        <div class="subscribe-block__item">
+            <div class="subscribe-block__title-first">SUBScRIBE</div>
+            <div class="subscribe-block__title-second">FOR OUR NEWLETteR AND PROMOTION</div>
+            <form action="#" class="subscribe-block__input">
+                <input type="email" placeholder="Enter Your Email" class="subscribe-block__email">
+                <input type="submit" value="Subscribe" class="subscribe-block__button">
+            </form>
         </div>
-
     </div>
 </div>
