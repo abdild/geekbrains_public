054affe273014d50280c4842786175cc3948c744
unknown <abdullin@yandex.ru>
2021-06-11 15:01:48 +0300

    lesson_5

--- 2_02_Advanced_JS/lesson_5/script.js
+++ 2_02_Advanced_JS/lesson_5/script.js
@@ -2,37 +2,59 @@ window.onload = () => {
     const App = {
         data() {
             return {
-                API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
+                API: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
                 catalogURL: '/catalogData.json',
-                products =[],
-                img = 'https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png'
+                goods: [],
+                filteredGoods: [],
+                searchLine: '',
+                imgCatalog: 'https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png',
+                isVisibleCart: false
             }
         },
         methods: {
-            // getJSON(url) {
-            //     return fetch(url ? url : `${this.API + this.catalogURL}`)
-            //         .then(result => result.json)
-            // },
-            async fetchGoods(url) {
-                // const response = await fetch(`${this.API + this.url}`);
-                // const data = await response.json();
-                // this.goods = data;
-                // console.log(this.goods);
-                try {
-                    const response = await fetch(url);
-                    const data = await response.json();
-                    this.goods = data;
+            async getProducts(url) {
+                const response = await fetch(url);
+                if (response.ok) {
+                    const catalogItems = await response.json();
+                    this.goods = catalogItems;
+                    this.filteredGoods = catalogItems;
+                } else {
+                    alert('Ошибка соединения с сервером');
                 }
-                catch(error) {
-                    console.log(`Ошибка ${error}`)
-                }
+            },
+            filterGoods(searchLine) {
+                const regExp = new RegExp(searchLine, 'i');
+                this.filteredGoods = this.goods.filter(good => regExp.test(good.product_name));
+            },
+            addProduct(good) {
+                console.log(good)
+                console.log(good.id_product)
             }
+            // async getJSON(url) {
+            //     try {
+            //         const response = await fetch(url);
+            //         const result = await response.json();
+            //         return result;
+            //     }
+            //     catch (error) {
+            //         console.log(`Ошибка ${error}`);
+            //     }
+            // },
+            // addProduct(el) {
+            //     console.log(el)
+            //     console.log(el.id_product)
+            // }
         },
-        mounted() {
-            this.fetchGoods(`${this.API + this.catalogURL}`)
-            console.log(this.goods)
+        async mounted() {
+            await this.getProducts(`${this.API + this.catalogURL}`)
+            // this.getJSON(`${this.API + this.catalogURL}`)
+            //     .then(data => {
+            //         for (let el of data) {
+            //             this.goods.push(el)
+            //         }
+            //     })
         }
     }
 
-    Vue.createApp(App).mount('#app')
+    Vue.createApp(App).mount('#app');
 }
